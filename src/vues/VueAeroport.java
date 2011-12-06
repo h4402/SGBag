97e426d1b8b9900218c134a339dc836fc97a104d
jtuloup <jeremy.tuloup@gmail.com>
2011-12-06 17:14:33 +0100

    Ajout d'elements d'ihm

--- src/vues/VueAeroport.java
+++ src/vues/VueAeroport.java
@@ -1,7 +1,25 @@
 package vues;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class VueAeroport extends JPanel {
 
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponents(g);
+		// TODO : a enlever
+		g.setColor(Color.white);
+		// On efface l'écran
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g.setColor(Color.black);
+		g.drawOval(50, 50, 50, 50);
+		g.fillOval(150, 150, 50, 50);
+		g.drawRect(200, 50, 200, 200);
+		
+		
+	}
+	
 }
