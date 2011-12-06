659e050e7afef495dbb8af365041fc6d649c1c90
Dorvaryn <benjamin.augustin@gmail.com>
2011-12-06 16:01:39 +0100

    Clic sur Guichet et Toboggan version simple

--- src/vues/VueAeroport.java
+++ src/vues/VueAeroport.java
@@ -2,6 +2,26 @@ package vues;
 
 import javax.swing.JPanel;
 
-public class VueAeroport extends JPanel {
+import noyau.Guichet;
 
+public class VueAeroport extends JPanel {
+	
+		private Guichet guichetCourant;
+		private int echelle;
+
+		public int getEchelle() {
+			return echelle;
+		}
+
+		public void setEchelle(int echelle) {
+			this.echelle = echelle;
+		}
+
+		public Guichet getGuichetCourant() {
+			return guichetCourant;
+		}
+
+		public void setGuichetCourant(Guichet guichetCourant) {
+			this.guichetCourant = guichetCourant;
+		}
 }
