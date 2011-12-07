8f263492745a4c5657e5a1e5826c04254a5cfd12
Moises Baly <moises.baly@gmail.com>
2011-12-07 10:06:23 +0100

    10:06 am

---
+++ src/vues/VueNoeud.java
@@ -0,0 +1,29 @@
+package vues;
+
+public class VueNoeud extends Vue{
+
+	public VueNoeud(VueGeneral vueGeneral) {
+		super(vueGeneral);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	boolean clic(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void dessin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void action() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
