e3ce839d17414f7a3c1db6f352eb4567dcb3f90e
ychevalier <yann.m.chevalier@gmail.com>
2011-12-13 23:31:06 +0100

    Correction du bug de Delphine!!

--- src/noyau/Guichet.java
+++ src/noyau/Guichet.java
@@ -73,23 +73,15 @@ public class Guichet {
 	 * et on ajoute le bagage de parametre en fin de file.
 	 */
 	public void ajoutBagage(Bagage b) {
-		Bagage bPrio = listBagages.peek();
-		if(bPrio == null) {
-			if(b != null) {
-				if(!tapis.ajouterBagage(b)) {
-					listBagages.offerLast(b);
-				}
-			}
+		if(b != null) {
+			listBagages.offerLast(b);
 		}
-		else {
-			if(tapis.ajouterBagage(bPrio)) {
-				if(b != null) {
-					listBagages.offerLast(b);
-				}
+		Bagage bPrio = listBagages.peek();
+		if(bPrio != null) {
+			if(tapis.ajouterBagage(bPrio)) {	
 				listBagages.poll();
 			}
 		}
-		
 	}
 	
 	/**
