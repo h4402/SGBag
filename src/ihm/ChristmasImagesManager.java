5b28f725f1aaa5fe3ab270aced40a66c223fca20
jtuloup <jeremy.tuloup@gmail.com>
2011-12-13 18:41:56 +0100

    correction de ChristmasImagesManager et debut d'ajout de d'easter egg

--- src/ihm/ChristmasImagesManager.java
+++ src/ihm/ChristmasImagesManager.java
@@ -7,7 +7,7 @@ import java.awt.Toolkit;
 public class ChristmasImagesManager extends ImagesManager{
 
     
-	protected final String PATH_IMAGE = "res/img/christmas";
+	protected final String PATH_IMAGE = "res/img/christmas/";
 	
     public ChristmasImagesManager(Toolkit tk) {
     	super(tk);
